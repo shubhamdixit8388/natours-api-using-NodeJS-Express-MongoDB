1f2e2bf53c82085df4b48ab0966c60033b03deb8
Shubham <svdixit8388@gmail.com>
2021-09-02 21:30:00 +0530

    server feature separated

--- app.js
+++ app.js
@@ -21,7 +21,4 @@ app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
 
 // Start Server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`)
-});
+module.exports = app;
