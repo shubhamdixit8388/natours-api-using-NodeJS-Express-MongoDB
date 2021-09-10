e3857bad27b9ca46b0ff63745fd4257809c83295
Shubham <svdixit8388@gmail.com>
2021-09-10 17:44:59 +0530

    mongoose package added and application connected to mongodb atlas

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 dotenv.config({path: `${__dirname}/config.env`});
 
 const app = require('./app');
 // console.log(process.env);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+    .then(() => console.log('Database connection succeed!!!'))
+    .catch(() => console.log('Database connection Failed!!!'))
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
