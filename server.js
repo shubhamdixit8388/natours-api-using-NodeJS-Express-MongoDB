08a143bd65e1bf5e8efbf17167cb743c83929880
Shubham <svdixit8388@gmail.com>
2021-09-11 15:41:03 +0530

    sample tour model added and saved to database

--- server.js
+++ server.js
@@ -13,7 +13,39 @@ mongoose.connect(DB, {
   useUnifiedTopology: true
 })
     .then(() => console.log('Database connection succeed!!!'))
-    .catch(() => console.log('Database connection Failed!!!'))
+    .catch(() => console.log('Database connection Failed!!!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    // required: true
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 499
+});
+
+testTour.save().then(doc => {
+  console.log(doc);
+}).catch(error => {
+  console.log(error);
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
