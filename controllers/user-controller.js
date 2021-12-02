965be0d52a3e1587b7c3e7ed50ee35cac62fd346
Shubham <svdixit8388@gmail.com>
2021-12-02 22:31:26 +0530

    signup user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,6 @@
+const catchAsync = require('../utils/catch-async');
+const User = require('../models/user-model');
+
 exports.getAllUsers = (req, res) => {
   res.status(500).send({
     status: 'Invalid',
@@ -12,12 +15,20 @@ exports.getUserById = (req, res) => {
   });
 };
 
-exports.addNewUser = (req, res) => {
-  res.status(500).send({
-    status: 'Invalid',
-    message: 'THis API is not implemented'
+exports.addNewUser = catchAsync(async(req, res) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
   });
-};
+  res.status(201).send({
+    status: 'success',
+    data: {
+      user: newUser
+    }
+  });
+});
 
 exports.updateUser = (req, res) => {
   res.status(500).send({
