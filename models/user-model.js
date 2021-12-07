cd67aee6a3cde77cc0664854b02d8c13ecab4685
Shubham <svdixit8388@gmail.com>
2021-12-07 10:14:30 +0530

    136.2 - reset password passwordChangedAt

--- models/user-model.js
+++ models/user-model.js
@@ -56,6 +56,12 @@ userSchema.pre('save', async function (next) {
   next();
 })
 
+userSchema.pre('save', function (next) {
+  if(!this.isModified('password') || this.isNew) return next();
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+})
+
 // Instance Mehtod
 userSchema.methods.checkPassword = async (candidatePassword, userPassword) => {
   return await bcrypt.compare(candidatePassword, userPassword);
