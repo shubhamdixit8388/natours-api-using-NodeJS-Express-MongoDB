b81e02cb48c48b2e536c0f8b99d1e88d8260387e
Shubham <svdixit8388@gmail.com>
2021-12-09 23:04:12 +0530

    149 - modelling tour guides - embedding

--- models/tour-model.js
+++ models/tour-model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./user-model');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -85,7 +86,8 @@ const tourSchema = new mongoose.Schema({
       description: String,
       day: Number
     }
-  ]
+  ],
+  guides: Array
 }, {toJSON: {virtuals: true}, toObject: {virtuals: true}});
 
 tourSchema.virtual('durationInWeeks').get(function () {
@@ -96,7 +98,15 @@ tourSchema.virtual('durationInWeeks').get(function () {
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, {lower: true})
   next();
-})
+});
+
+// Embedding in mongoDB
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// })
+
 tourSchema.post('save', function (document, next) {
   // console.log(document);
   next();
