8fc0a34663a9505b162af5ba1ab04087f1b8f890
Shubham <svdixit8388@gmail.com>
2021-12-11 23:09:57 +0530

    162 - adding missing authentication and authorization

--- routes/review-routes.js
+++ routes/review-routes.js
@@ -5,15 +5,17 @@ const authController = require('../controllers/auth-controller');
 // Using mergeParams we can can upper route params like tourId in current params method createReview
 const router = express.Router({mergeParams: true});
 
+// Creating middleware of authenticate user so that it will applicable to all API's below this middleware
+router.use(authController.authenticateUser);
+
 // POST /tours/<tourId>/reviews
 router.route('')
-    .get(authController.authenticateUser, reviewController.getAllReviews)
-    .post(authController.authenticateUser, reviewController.setTourUserId, reviewController.createReview);
+    .get(reviewController.getAllReviews)
+    .post(authController.checkUserRole('admin'), reviewController.setTourUserId, reviewController.createReview);
 
 router.route('/:id')
-    .get(authController.authenticateUser, reviewController.getReviewById)
-    .delete(authController.authenticateUser, authController.checkUserRole('admin'),
-    reviewController.deleteReviewById)
-    .patch(authController.authenticateUser, reviewController.updateReviewById);
+    .get(reviewController.getReviewById)
+    .delete(authController.checkUserRole('admin', 'lead-guide'), reviewController.deleteReviewById)
+    .patch(authController.checkUserRole('admin', 'lead-guide'), reviewController.updateReviewById);
 
 module.exports = router;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -8,23 +8,25 @@ router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 router.route('/forgot-password').post(authController.forgotPassword);
 router.route('/reset-password/:token').patch(authController.resetPassword);
-router.route('/update-password').patch(authController.authenticateUser, authController.updatePassword);
 
-router.route('/me').patch(authController.authenticateUser, userController.updateMe);
-router.route('/me').delete(authController.authenticateUser, userController.deleteMe);
-router.route('/me').get(authController.authenticateUser, userController.getMe,
-    userController.getUserById)
+// Creating middleware of authenticate user so that it will applicable to all API's below this middleware
+router.use(authController.authenticateUser)
+
+router.route('/update-password').patch(authController.updatePassword);
+
+router.route('/me').patch(userController.updateMe);
+router.route('/me').delete(userController.deleteMe);
+router.route('/me').get(userController.getMe, userController.getUserById)
+
+// Creating middleware to check user is admin or not, If user is admin then we allow user to do following actions
+router.use(authController.checkUserRole('admin'))
 
 router.route('/')
-    .get(authController.authenticateUser, authController.checkUserRole('admin'),
-        userController.getAllUsers)
-    .post(authController.authenticateUser, authController.checkUserRole('admin'),
-        userController.addNewUser);
+    .get(userController.getAllUsers)
+    .post(userController.addNewUser);
 router.route('/:id')
-    .get(authController.authenticateUser, userController.getUserById)
-    .patch(authController.authenticateUser, authController.checkUserRole('admin'),
-        userController.updateUser)
-    .delete(authController.authenticateUser, authController.checkUserRole('admin'),
-        userController.deleteUserById);
+    .get(userController.getUserById)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUserById);
 
 module.exports = router;
