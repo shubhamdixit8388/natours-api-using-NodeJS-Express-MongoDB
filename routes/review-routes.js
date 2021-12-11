be72eca64f7040da4017374ec2475ff74dc8f930
Shubham <svdixit8388@gmail.com>
2021-12-11 11:45:21 +0530

    158 - nested routes with express

--- routes/review-routes.js
+++ routes/review-routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const reviewController = require('../controllers/review-controller');
 const authController = require('../controllers/auth-controller');
 
-const router = express.Router();
+// Using mergeParams we can can upper route params like tourId in current params method createReview
+const router = express.Router({mergeParams: true});
 
+// POST /tours/<tourId>/reviews
 router.route('')
     .get(authController.authenticateUser, reviewController.getAllReviews)
     .post(authController.authenticateUser, authController.checkUserRole('user'),
