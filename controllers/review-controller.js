876fca37e61c8f464a6a95fc3bbfbbda5145850f
Shubham <svdixit8388@gmail.com>
2021-12-11 12:22:21 +0530

    159 - get all reviews for specific tour

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -2,7 +2,9 @@ const Review = require('../models/review-model');
 const catchAsync = require('../utils/catch-async');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = {tour: req.params.tourId};
+  const reviews = await Review.find(filter);
 
   res.status(200).send({
     status: 'success',
