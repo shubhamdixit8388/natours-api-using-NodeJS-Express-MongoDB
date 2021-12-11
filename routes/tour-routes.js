f528c022670436bcfe8ea12b15ddb1a82f91d9a0
Shubham <svdixit8388@gmail.com>
2021-12-11 11:45:21 +0530

    158 - nested routes with express

--- routes/tour-routes.js
+++ routes/tour-routes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const toursController = require('../controllers/tour-controller')
 const authController = require('../controllers/auth-controller')
-const reviewController = require('../controllers/review-controller')
+const reviewRoutes = require('../routes/review-routes')
 
 const router = express.Router();
 
-// router.param('id', toursController.checkTourId);
+// GET /tours/<tourId>/reviews
+// GET /tours/<tourId>/reviews/<userId>
+
+// router.route('/:tourId/reviews')
+//     .post(authController.authenticateUser, authController.checkUserRole('user'),
+//         reviewController.createReview);
+
+router.use('/:tourId/reviews', reviewRoutes)
 
 router.route('/top-5-cheap').get(toursController.aliasTopTours, toursController.getAllTours)
 
@@ -16,13 +23,5 @@ router.route('/:id')
     .get(toursController.getTourById)
     .patch(toursController.updateTour)
     .delete(authController.authenticateUser, authController.checkUserRole('admin', 'lead-guide'), toursController.deleteTourById);
-
-// POST /tours/<tourId>/reviews
-// GET /tours/<tourId>/reviews
-// GET /tours/<tourId>/reviews/<userId>
-
-router.route('/:tourId/reviews')
-    .post(authController.authenticateUser, authController.checkUserRole('user'),
-        reviewController.createReview);
 
 module.exports = router;
