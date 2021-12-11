3503b3105028370ebf088c06174eff76afd62dce
Shubham <svdixit8388@gmail.com>
2021-12-11 22:31:56 +0530

    162 - removed all previous methods for CRUD operations in controllers and added new factory methods for code reuse

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -1,7 +1,4 @@
 const Tour = require('../models/tour-model');
-const APIFeatures = require('./../utils/api-features');
-const catchAsync = require('../utils/catch-async');
-const AppError = require('../utils/app-error');
 const factory = require('./handler-factory');
 
 exports.aliasTopTours = (req, res, next) => {
@@ -11,92 +8,11 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 }
 
-/*exports.getAllTours = catchAsync(async (req, res, next) => {
-  const featuresApi = new APIFeatures(Tour.find(), req.query)
-      .filter()
-      .sort()
-      .fieldSelection()
-      .pagination();
-  // Execute query
-  const tours = await featuresApi.query;
-
-  // Chaining to query
-  // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
-
-  // Send response
-  res.status(200).send({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
-});*/
-
-exports.getAllTours = factory.getAll(Tour);
-
-// exports.getTourById = catchAsync(async (req, res, next) => {
-//   // const tour = await Tour.findOne({_id: req.params.id});
-//   // const tour = await Tour.findById(req.params.id).populate('guides');
-//   const tour = await Tour.findById(req.params.id).populate('guides');
-//
-//   if(!tour) {
-//      return next(new AppError('No tour found with this id', 404));
-//   }
-//
-//   res.status(200).send({
-//     status: 'success',
-//     data: {
-//       tour: tour
-//     }
-//   });
-// });
-
-exports.getTourById = factory.getOne(Tour, ['reviews', {
-                        path: 'guides',
-                        select: '-__v -passwordChangedAt'
-                      }]);
-
 exports.addNewTour = factory.addOne(Tour);
+exports.getTourById = factory.getOne(Tour, ['reviews', {
+  path: 'guides',
+  select: '-__v -passwordChangedAt'
+}]);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTourById = factory.deleteOne(Tour);
-
-// exports.addNewTour = catchAsync(async (req, res, next) => {
-//   const newTour = await Tour.create(req.body);
-//   res.status(201).send({
-//     status: 'success',
-//     data: {
-//       tour: newTour
-//     }
-//   });
-// });
-
-// exports.updateTour = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-//     new: true,
-//     runValidators: true
-//   });
-//
-//   if(!tour) {
-//     return next(new AppError('No tour found with this id', 404));
-//   }
-//
-//   res.status(200).send({
-//     status: 'success',
-//     data: {
-//       tour
-//     }
-//   });
-// });
-
-// exports.deleteTourById = catchAsync(async (req, res, next) => {
-//   const tour = await Tour.findByIdAndDelete(req.params.id);
-//
-//   if(!tour) {
-//     return next(new AppError('No tour found with this id', 404));
-//   }
-//
-//   res.status(204).send({
-//     status: 'success'
-//   });
-// });
+exports.getAllTours = factory.getAll(Tour);
