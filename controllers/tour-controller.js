29bb18a75c6157e758a2ea74878a2a9a8263ad49
Shubham <svdixit8388@gmail.com>
2021-12-14 22:58:48 +0530

    203 - 205 - uploading and procesing multiple tour images

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -1,7 +1,57 @@
+const multer = require('multer');
+const sharp = require('sharp');
+
 const Tour = require('../models/tour-model');
 const factory = require('./handler-factory');
 const AppError = require('../utils/app-error');
 const catchAsync = require('../utils/catch-async');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, callback) => {
+  if (file.mimetype.startsWith('image')) {
+    callback(null, true)
+  } else {
+    callback(new AppError('Please upload only image file', 400), false);
+  }
+  callback(null, 'public/img/users');
+}
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+// upload.single('imageCover'); -> req.file
+// upload.array('images', 5); -> req.files
+exports.uploadTourPhotos = upload.fields([
+  {name: 'imageCover', maxCount: 1},
+  {name: 'images', maxCount: 3}
+]);
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+  // 1. Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+      .resize(2000, 1333)
+      .toFormat('jpeg')
+      .jpeg({quality: 90})
+      .toFile(`public/img/tours/${req.body.imageCover}`);
+  req.body.images = [];
+  await Promise.all(
+      req.files.images.map(async (file, index) => {
+        const fileName = `tour-${req.params.id}-${Date.now()}-${index + 1}.jpeg`;
+        await sharp(file.buffer)
+            .resize(2000, 1333)
+            .toFormat('jpeg')
+            .jpeg({quality: 90})
+            .toFile(`public/img/tours/${fileName}`);
+        req.body.images.push(fileName);
+      })
+  )
+  next();
+});
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
