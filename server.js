b18b40fa3e701783471f0669f5219baa8479456f
Shubham <svdixit8388@gmail.com>
2021-12-18 17:40:50 +0530

    225 - console added to see running port number on heroku

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(DB, {
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  // console.log(`App running on port ${port}...`)
+  console.log(`App running on port ${port}...`)
 });
 
 process.on('unhandledRejection', err => {
